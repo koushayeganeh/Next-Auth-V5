124ab2d1fbfe387c19460ead5d223e686c7cc334
Kousha Yeganeh <koushayeganeh@gmail.com>
2024-07-18 18:03:56 +0300

    Home page and Login Form

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Header } from "@/components/auth/header";
+import { Social } from "@/components/auth/social";
+import { BackButton } from "@/components/auth/back-button";
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLable: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonLable,
+  backButtonHref,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel}></Header>
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+      <CardFooter className="items-center justify-center">
+        <BackButton label={backButtonLable} href={backButtonHref} />
+      </CardFooter>
+    </Card>
+  );
+};
