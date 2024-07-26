e793efd26a8c7034f47af75f26bbc66892d0bbb0
Kousha Yeganeh <koushayeganeh@gmail.com>
2024-07-26 11:13:37 +0300

    added role to the user schema, auth.js callbacks

---
+++ next-auth-d.ts
@@ -0,0 +1,15 @@
+//This file is for declaring the type for user roll (or any other future cutome fields) used in auth.js
+
+import { UserRole } from "@prisma/client";
+import NextAuth, { type DefaultSession } from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  //customFiled: String
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
